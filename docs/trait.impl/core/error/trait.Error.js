7f3bb2a36acab537b2a2b3a042ba270af18258ff
Johann Saunier <johann@scrapfly.io>
2026-09-08 14:24:52 +0000

    Release 0.5.1

--- docs/trait.impl/core/error/trait.Error.js
+++ docs/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["scrapfly_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"scrapfly_sdk/error/enum.ScrapflyError.html\" title=\"enum scrapfly_sdk::error::ScrapflyError\">ScrapflyError</a>",0]]]]);
+    const implementors = Object.fromEntries([["scrapfly_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"scrapfly_sdk/error/enum.ScrapflyError.html\" title=\"enum scrapfly_sdk::error::ScrapflyError\">ScrapflyError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/futures_core/stream/trait.Stream.js
+++ docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["scrapfly_sdk",[["impl&lt;S&gt; Stream for <a class=\"struct\" href=\"scrapfly_sdk/batch/struct.BatchPartStream.html\" title=\"struct scrapfly_sdk::batch::BatchPartStream\">BatchPartStream</a>&lt;S&gt;<div class=\"where\">where\n    S: Stream&lt;Item = <a class=\"enum\" href=\"https://doc.rust-lang.org/1.95.0/core/result/enum.Result.html\" title=\"enum core::result::Result\">Result</a>&lt;Bytes, Error&gt;&gt; + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>,</div>",0]]]]);
+    const implementors = Object.fromEntries([["scrapfly_sdk",[["impl&lt;S&gt; Stream for <a class=\"struct\" href=\"scrapfly_sdk/batch/struct.BatchPartStream.html\" title=\"struct scrapfly_sdk::batch::BatchPartStream\">BatchPartStream</a>&lt;S&gt;<div class=\"where\">where\n    S: Stream&lt;Item = <a class=\"enum\" href=\"https://doc.rust-lang.org/1.98.1/core/result/enum.Result.html\" title=\"enum core::result::Result\">Result</a>&lt;Bytes, Error&gt;&gt; + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>,</div>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
